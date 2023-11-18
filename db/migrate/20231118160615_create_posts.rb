24993a376264658a3ab0376227379b9f907a3ef9
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-18 12:02:05 -0500

    Setup devise successfully.

--- db/migrate/20231118160615_create_posts.rb
+++ db/migrate/20231118160615_create_posts.rb
@@ -1,8 +1,8 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
-      t.string :title
-      t.string :body
+      t.string :title, null: false
+      t.string :body, null: false
       t.references :user, null: false, foreign_key: true
 
       t.timestamps
