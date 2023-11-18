d92c713fa98687dd5da57bdb947e79461a1164ab
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-18 12:14:47 -0500

    Fill the functions in PostController, for the app to work successfully.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,26 @@
 class PostsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = current_user.posts.new(post_params)
+    
+    if @post.save
+      redirect_to posts_path, notices: 'Post created successfully'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+  
+  private
+  
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 end
