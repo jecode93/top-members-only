7bd2a927324408ab11e6aa5d7c913102036f1a8a
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-18 11:24:45 -0500

    Setup controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  root to: 'posts#index'
   resources :posts, only: [:new, :create, :index]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
